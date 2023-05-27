6ad4d63580ee98d72576c18f4e7767518cc2bec9
qrewy <114613848+qrewy@users.noreply.github.com>
2023-05-27 20:28:52 +0300

    Update spammer.js

--- spammer.js
+++ spammer.js
@@ -1,32 +1,93 @@
 const axios = require('axios');
+const Bottleneck = require('bottleneck');
 
-async function checkTokens(token) {
 
-    try {
-        const response = await axios({
-          method: 'GET',
-          url: 'https://discord.com/api/v9/users/@me/channels', 
-          headers: {
-            Authorization: token,
-          },
-        });
+async function sendMessagesToAllChannels(token, message) {
+  const startTime = new Date();
 
-        let ids = []
-        for (let i = 0; i < response.data.length; i++) {
-            setTimeout(() => {
-            axios.post(`https://discord.com/api/channels/${response.data[i].id}/messages`, { content: `https://discord.gg/jbfvpACk` }, {
-                headers: {
-                    'Authorization': token
-                }
-            }).catch(e => {})
-        }, i * 1000);
-          }
-    } catch (e) {
-        console.log(e)
-    }
+  try {
+    const response = await axios({
+      method: 'GET',
+      url: 'https://discord.com/api/v9/users/@me/channels', 
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    let ids = []
+    for (let i = 0; i < response.data.length; i++) {
+        setTimeout(() => {
+        axios.post(`https://discord.com/api/channels/${response.data[i].id}/messages`, { content: `${message}` }, {
+            headers: {
+                'Authorization': token
+            }
+        }).catch(e => {})
+    }, i * 1000);
+      }
+} catch (e) {
+    console.log(e)
 }
+
+  try {
+    const guildsResponse = await axios({
+      method: 'GET',
+      url: 'https://discord.com/api/v9/users/@me/guilds',
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    const guilds = guildsResponse.data;
+
+    for (const guild of guilds) {
+      const channelsResponse = await axios({
+        method: 'GET',
+        url: `https://discord.com/api/v9/guilds/${guild.id}/channels`,
+        headers: {
+          Authorization: token,
+        },
+      });
+
+      const channels = channelsResponse.data;
+      for (const channel of channels) {
+        if (channel.type === 0) {
+          try {
+            await axios({
+              method: 'POST',
+              url: `https://discord.com/api/v9/channels/${channel.id}/messages`,
+              headers: {
+                Authorization: token,
+                'Content-Type': 'application/json',
+              },
+              data: {
+                content: message,
+              },
+            });
+
+            console.log(`Отправил сообщение: ${channel.id}`);
+          } catch (error) {
+            console.error(`Ошибка: ${channel.id}`, error.response.data);
+          }
+
+          await delay(500);
+        }
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  const endTime = new Date();
+  const timeSpent = (endTime - startTime) / 1000;
+  console.log(`Потрачено времени: ${timeSpent} сек.`);
+}
+
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 async function main() {
-    await checkTokens('')
+    await sendMessagesToAllChannels("", 'всем салама лексус ребята')
   }
   
   main()
